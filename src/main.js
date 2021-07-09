180683fd863bf8a1285dbc0203061e58960d1b51
Kami-Kaze <18683538+KanjiuAkuma@users.noreply.github.com>
2021-07-09 11:35:34 +0200

    enable timing on main

--- src/main.js
+++ src/main.js
@@ -88,7 +88,7 @@ function animate(timestamp) {
     ctx.beginFrame();
 
     ctx.setComposition('source-over');
-    const dt = timer.update(timestamp); // 0.016;
+    const dt = timer.update(timestamp);
 
     entities.forEach(function (entity, _) {
         entity.update(dt, viewBounds);
